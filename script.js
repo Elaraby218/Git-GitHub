0ef20c31fa4769e310f7786c637913e824a16f63
abdo0-0ayman <abdoayman9@gmail.com>
2024-08-01 13:32:10 +0300

    add student

--- script.js
+++ script.js
@@ -1,12 +1,21 @@
-let arr =[] ; 
+let arr = [];
 arr.push({
-    Name : "Elaraby" , 
-    Email: "elo@gmail.com" , 
-    Age  : 21 , 
-    
-    fullData : function (){
-        console.log(this.Name , this.Age , this.Email) ; 
-    }
-}) ;
+        Name: "Elaraby",
+        Email: "elo@gmail.com",
+        Age: 21,
 
+        fullData: function () {
+                console.log(this.Name, this.Age, this.Email);
+        }
+});
+arr.push({
+        Name: "Abdo",
+        Email: "abdoayman@gmail.com",
+        Age: 21,
+        fullData: function () {
+                console.log(this.Name, this.Age, this.Email);
+        }
+});
 
+arr[0].fullData();
+arr[1].fullData();
