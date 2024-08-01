935bbb1f43e559772e06642a38949da87e2ca259
Abdo-hany48 <abdelrhmanhany200348@gmail.com>
2024-08-01 14:37:03 +0300

    AbdoHany changes

--- script.js
+++ script.js
@@ -16,6 +16,16 @@ arr.push({
                 console.log(this.Name, this.Age, this.Email);
         }
 });
+arr.push({
+    Name:"Abdo Hany",
+    Email:"abdelrhmanhany200348@gmail.com",
+    Age:21,
+    
+    fullData: function(){
+        console.log(this.Name,this.Age,this.Email);
+    }
+})
 
 arr[0].fullData();
 arr[1].fullData();
+arr[2].fullData();
