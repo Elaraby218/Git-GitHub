c2c3c81ab0424bef8e8c5def8917771858fe89fc
Elaraby218 <152742814+Elaraby218@users.noreply.github.com>
2024-08-01 14:46:44 +0300

    object added to the local storage

--- script.js
+++ script.js
@@ -29,3 +29,11 @@ arr.push({
 arr[0].fullData();
 arr[1].fullData();
 arr[2].fullData();
+
+let s1 = JSON.stringify(arr[0]) ; 
+let s2 = JSON.stringify(arr[1]) ;
+let s3 = JSON.stringify(arr[2]) ;
+
+window.localStorage.user1 = s1 ; 
+window.localStorage.user2 = s1 ; 
+window.localStorage.user3 = s1 ; 
