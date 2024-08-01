5a21a531c4c6ec0f41de93ecb39569209f0f8653
Elaraby218 <152742814+Elaraby218@users.noreply.github.com>
2024-08-01 13:05:57 +0300

    elaraby push

---
+++ script.js
@@ -0,0 +1,12 @@
+let arr =[] ; 
+arr.push({
+    Name : "Elaraby" , 
+    Email: "elo@gmail.com" , 
+    Age  : 21 , 
+    
+    fullData : function (){
+        console.log(this.Name , this.Age , this.Email) ; 
+    }
+}) ;
+
+
